f55e0875b0881d97ab23f3e54b5c8134faf8ca43
Mate Wolf <Mate_Wolf@epam.com>
2018-01-26 14:53:46 +0100

    added H2 in-memory dataabse settings to properties + initializer SQL script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into team (team_name) values ('real');
+insert into team (team_name) values ('fictional');
+
+insert into player (first_name, last_name, points, team_id) values ('Mate', 'Wolf', 10000000, 1);
+insert into player (first_name, last_name, points, team_id) values ('Luke', 'Skywalker', 1977, 2);
+insert into player (first_name, last_name, points, team_id) values ('Marty', 'McFly', 2015, 2);
